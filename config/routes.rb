c3b430defe3f5d79e3707ec8fe4236263980a82f
Eric Park <erichoonpark@gmail.com>
2015-07-16 11:36:31 -0700

    Refactor Code

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :topics do
      resources :posts, except: [:index]
    end
+   
+   resources :users, only: [:update]
 
   devise_for :users
   get 'welcome/index'
